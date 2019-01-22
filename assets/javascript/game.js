b12ca2b6d96daa40f7b38cd79d33e8e09ec68492
Mr_Cheese <kevinprovost776@gmail.com>
2019-01-21 22:33:45 -0600

    more js updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,71 @@
 $(document).ready(function(){
-    //genertaing random number for the user score and adding to html
+   
+    $("#start").on("click",function(gameStart){
+    
+        
+        
+        
     var gameScore = Math.floor(Math.random() * 100+7)
     console.log(gameScore)
-
-    $(".gameScoreText").text(gameScore);
-
-    var crystals = {
-        numOne: Math.floor(Math.random(10*3)),
-        numTwo: Math.floor(Math.random(10*3)),
-        numThree: Math.floor(Math.random(10*3)),
-        numFour: Math.floor(Math.random(10*3)),
-    }
+    var crystal1 = Math.floor(Math.random() * 10+3);
+    var crystal2 = Math.floor(Math.random() * 8+3);
+    var crystal3 = Math.floor(Math.random() * 14+3);
+    var crystal4 = Math.floor(Math.random() * 9+5);
     var userTotal = 0;
     var wins = 0;
     var losses = 0;
 
+    $(".gameScoreText").text(gameScore);
+    $(".userScoreText").text(userTotal);
     $("#wins").text(wins);
     $("#losses").text(losses);
+    $("#start").hide();
 
-    console.log(wins,losses)
+    $("#crystal1").attr("value", crystal1);
+    $("#crystal2").attr("value", crystal2);
+    $("#crystal3").attr("value", crystal3);
+    $("#crystal4").attr("value", crystal4);
+
+    $("#crystal1").on("click", function(){
+        console.log("clicked")
+        var value1 = $(this).attr("value");
+        console.log(value1)
+        var n = 0;
+        newval = n+value1;
+            $(".userScoreText").text($('<p/>', {text: newval}));     
+    })
+    $("#crystal2").on("click", function(){
+
+        console.log("clicked")
+        var value2 = $(this).attr("value");
+        console.log(value2)
+    })
+    $("#crystal3").on("click", function(){
+
+        console.log("clicked")
+        var value3 = $(this).attr("value");
+        console.log(value3)
+    })
+    $("#crystal4").on("click", function(){
+
+        console.log("clicked")
+        var value4 = $(this).attr("value");
+        console.log(value4)
+    })
+
+    })
+
     
-    function reset(){
-        var gameScore = Math.floor(Math.random() * 100+7)
-            console.log(gameScore)
-        $(".gameScoreText").text(gameScore);
-        crystals = Math.floor(Math.random(10*3));
-        userTotal = 0;
-        $("#userScoreText").text(userTotal);
-    }   
+
+
+    //genertaing random number for the user score and adding to html
+    
+
+
+    
+    
+        
+     
         //============================================================================================================
     //     // Functions for scores 
     // function startGame() {
